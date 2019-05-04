e67d8acde3c8931e136babdce85ba12219b6027b
Sreejith Pp <mailz4sreejith@gmail.com>
2019-05-04 17:09:32 +0530

    Added dotenv and maps

--- android/app/src/main/java/com/native_app_template/MainApplication.java
+++ android/app/src/main/java/com/native_app_template/MainApplication.java
@@ -3,6 +3,7 @@ package com.native_app_template;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
 import com.wix.interactable.Interactable;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
             new Interactable(),
             new ReactMaterialKitPackage(),
             new RNGestureHandlerPackage()
