24a28716e7021855a085319734c407051515a16b
Sreejith Pp <mailz4sreejith@gmail.com>
2019-05-04 17:42:00 +0530

    Added map screen

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const RootStack = createStackNavigator(
     About
   },
   {
-    initialRouteName: 'Map'
+    initialRouteName: 'Home'
   }
 );
 
--- src/screens/Map.js
+++ src/screens/Map.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+ container: {
+   height: 400,
+   width: 400,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+ },
+});
 
 class Map extends React.Component {
   render() {
-    return <MapView />
+    return (
+      <View style={styles.container}>
+        <Text>lol</Text>
+      </View>
+    )
   }
 }
-
 
 export default Map;
