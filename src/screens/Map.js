2a0ed098707f69c7be15961ef02412dfbb92904f
Sreejith Pp <mailz4sreejith@gmail.com>
2019-05-04 23:14:40 +0530

    Remove unnecessary animation in Map screen

--- src/screens/Map.js
+++ src/screens/Map.js
@@ -43,38 +43,6 @@ class MapScreen extends React.Component {
       }
     ]
   }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        journey: [
-          {
-            entity: {
-              coordinate: {
-                latitude: 12.941093,
-                longitude: 77.638987,
-              },
-              type: "PERSON",
-            },
-            route: [{
-              latitude: 12.941093,
-              longitude: 77.638987,
-            }, {
-              latitude: 13.001093,
-              longitude: 77.838987,
-            }],
-            time: {
-              coordinate: {
-                latitude: 13.001093,
-                longitude: 77.838987,
-              },
-              time: new Date()
-            }
-          }
-        ]
-      })
-    }, 2000);
-  }
   
   render() {
     return (
